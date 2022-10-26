dea6e5af6bbc7806ae1376b8863277e4f4eb6e3b
Lucas Hauszler <lucas.hauszler@epitech.eu>
2022-10-26 12:01:44 +0200

    feat: add smart highlighting of some coding style errors

--- src/utils.js
+++ src/utils.js
@@ -29,17 +29,58 @@ function getSeverityAsVSCode(severity) {
 }
 
 /**
+ * Tries to find the best range to highlight in the editor using specific rules.
+ * @param {*} file 
+ * @param {*} line 
+ * @param {*} severity 
+ * @param {*} language 
+ * @param {*} subgroup 
+ * @param {*} projectFile 
+ * @returns 
+ */
+function getSmartRange(file, line, severity, language, subgroup, projectFile) {
+	if (subgroup === "G7") {
+		var lineContent = projectFile.lines[line - 1];
+		var match = lineContent.match(/ +$/);
+
+		if (match != null) {
+			return new vscode.Range(line - 1, match.index, line - 1, lineContent.length);
+		}
+	}
+	if (subgroup === "F3") {
+		return new vscode.Range(line - 1, 79, line - 1, 69420);
+	}
+	if (subgroup === "L2") {
+		var lineContent = projectFile.lines[line - 1];
+		var match = lineContent.match(/^\s+/);
+
+		if (match != null) {
+			return new vscode.Range(line - 1, 0, line - 1, match.index + match[0].length);
+		}
+	}
+	return null;
+}
+
+/**
  * Returns the range for a given report error.
  * @param {string} file
  * @param {number} line
  * @param {string} severity
  * @param {string} language 
  * @param {string} subgroup 
+ * @param {Object} projectFile The project file object containing the file path and the file contents.
  * @returns {vscode.Range}
  */
-function getReportErrorRange(file, line, severity, language, subgroup) {
+function getReportErrorRange(file, line, severity, language, subgroup, projectFile) {
 	var endChar = 69420;
 
+	if (projectFile != undefined) {
+		var smartRange = getSmartRange(file, line, severity, language, subgroup, projectFile);
+
+		if (smartRange != null) {
+			return smartRange;
+		}
+	}
 	return new vscode.Range(line - 1, 0, line - 1, endChar);
 }
 
